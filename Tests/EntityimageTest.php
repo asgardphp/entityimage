027893d9d83b56762a32c02dc14079c496e5eb87
Michel Hognerud <leyou.m@gmail.com>
2014-08-28 13:19:24 +0200

    refactoring entity use of container

--- Tests/EntityimageTest.php
+++ Tests/EntityimageTest.php
@@ -5,19 +5,27 @@ class EntityimageTest extends \PHPUnit_Framework_TestCase {
 	public static function setUpBeforeClass() {
 		$container = new \Asgard\Container\Container;
 		$container->register('Asgard.Entity.PropertyType.image', function($container, $params) { return new \Asgard\Entityimage\ImageProperty($params); });
-		$container['config'] = new \Asgard\Config\Config;
-		$container['hooks'] = new \Asgard\Hook\HooksManager($container);
-		$container['cache'] = new \Asgard\Cache\NullCache;
-		$container['rulesregistry'] = new \Asgard\Validation\RulesRegistry;
+		$container['config']          = new \Asgard\Config\Config;
+		$container['hooks']           = new \Asgard\Hook\HooksManager($container);
+		$container['cache']           = new \Asgard\Cache\NullCache;
+		$container['rulesregistry']   = new \Asgard\Validation\RulesRegistry;
 		$container['rulesregistry']->registerNamespace('Asgard\File\Rules');
-		$container['entitiesmanager'] = new \Asgard\Entity\EntitiesManager($container);
-		\Asgard\Entity\Entity::setContainer($container);
+		$container->register('validator', function($container) {
+			$validator = new \Asgard\Validation\Validator;
+			$validator->setRegistry($container['rulesregistry']);
+			return $validator;
+		});
+
+		$entitiesManager = $container['entitiesmanager'] = new \Asgard\Entity\EntitiesManager($container);
+		$entitiesManager->setValidatorFactory($container->createFactory('validator'));
+		#set the EntitiesManager static instance for activerecord-like entities (e.g. new Article or Article::find())
+		\Asgard\Entity\EntitiesManager::setInstance($entitiesManager);
 	}
 
 	public function test() {
 		copy(__DIR__.'/Fixtures/fixture.jpg', __DIR__.'/Fixtures/image.jpg');
 		$ent = new Fixtures\EntityWithImage([
-			'name' => 'Entity',
+			'name'  => 'Entity',
 			'image' => __DIR__.'/Fixtures/image.jpg',
 		]);
 		$img = $ent->image;
